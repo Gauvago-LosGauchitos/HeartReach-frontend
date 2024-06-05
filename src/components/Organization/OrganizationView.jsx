38aaf52c349cc114f655f7dd6546d394dee83e14
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-06-04 19:59:47 -0600

    Arreglo de organizacion

--- src/components/Organization/OrganizationView.jsx
+++ src/components/Organization/OrganizationView.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './OrganizationView.css'
-import ImgDefault from '../../assets/img/logo.png'
-import { useNavigate } from 'react-router-dom'
-import { useOrganization } from '../../shared/hooks/useOrganization.jsx'
+import React from 'react';
+import './OrganizationView.css';
+import ImgDefault from '../../assets/img/logo.png';
+import { useNavigate } from 'react-router-dom';
+import { useOrganization } from '../../shared/hooks/useOrganization.jsx';
 
 export const OrganizationView = () => {
-  const { org, isLoading } = useOrganization()
-  const navigate = useNavigate()
+  const { org, isLoading } = useOrganization();
+  const navigate = useNavigate();
 
-  console.log('Organization in Showcase:', org)
+  console.log('Organization data:', org);
+
+  const organizations = org.organizations ? [org.organizations] : []; // Convertimos el objeto en un array para mapear
+
   return (
     <div className='body'>
-
       <div className="header_image_container">
         <div>
           <h1>Hola chitu</h1>
@@ -23,19 +25,14 @@ export const OrganizationView = () => {
         <section>
           <h2 className="section__header">Escoje tu proximo voluntariado</h2>
           <div>
-            {!isLoading && Array.isArray(org) && org.map((orgItem, index) => (
-              
+            {!isLoading && organizations.map((orgItem, index) => (
               <div key={index} style={{ border: '1px solid black' }}>
-
                 <div>
                   <img src={ImgDefault} alt="Organization Logo" />
                   <div>
                     <label>
                       <input type="checkbox" />
-                      {console.log(orgItem)}
-                      {console.log("ola")}
                     </label>
-
                   </div>
                 </div>
                 <div>
@@ -47,5 +44,5 @@ export const OrganizationView = () => {
         </section>
       </div>
     </div>
-  )
+  );
 }
