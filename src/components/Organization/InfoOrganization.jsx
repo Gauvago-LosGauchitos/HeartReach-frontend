8ea3579d4c50acf63e2b1094d8adaf7c1dd3c86d
dzabala-2019001 <dzabala-2019001@kinal.edu.gt>
2024-06-10 11:11:14 -0600

    Se mdofico la vista de Info Org

--- src/components/Organization/InfoOrganization.jsx
+++ src/components/Organization/InfoOrganization.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react';
 import './InfoOrganization.css';
-import ImgDefault from '../../assets/img/img1.jpg';
+import ImgDefault from '../../assets/img/bg.svg';
+import ImgWaos from '../../assets/img/ensalada-1.png'
+import timexD from '../../assets/img/time.svg'
+import UbixD from '../../assets/img/gps.svg'
+import PhonexD from '../../assets/img/phone.svg'
 import { useOrganization } from '../../shared/hooks/useOrganization';
 import { useParams } from 'react-router-dom';
+
+
+
+
 
 
 export const InfoOrganization = () => {
@@ -14,99 +22,54 @@ export const InfoOrganization = () => {
             await getOrgsId(id);
         };
         fetchData();
-    }, [id, getOrgsId]);
-
-    useEffect(() => {
-        // Este useEffect se asegura de que el DOM esté listo antes de intentar manipularlo
-        if (!isLoading && selectedOrg) {
-            const nextBtn = document.querySelector('.next');
-            const prevBtn = document.querySelector('.prev');
-            const slider = document.querySelector('.slider');
-            const sliderList = slider?.querySelector('.list');
-            const thumbnail = document.querySelector('.thumbnail');
-            const thumbnailItems = thumbnail?.querySelectorAll('.item');
-
-            if (thumbnailItems && thumbnailItems.length > 0) {
-                thumbnail.appendChild(thumbnailItems[0]);
-            }
-
-            const moveSlider = (direction) => {
-                const sliderItems = sliderList?.querySelectorAll('.item');
-                const thumbnailItems = document.querySelectorAll('.thumbnail .item');
-
-                if (sliderItems && thumbnailItems) {
-                    if (direction === 'next') {
-                        sliderList.appendChild(sliderItems[0]);
-                        thumbnail.appendChild(thumbnailItems[0]);
-                        slider.classList.add('next');
-                    } else {
-                        sliderList.prepend(sliderItems[sliderItems.length - 1]);
-                        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
-                        slider.classList.add('prev');
-                    }
-
-                    const handleAnimationEnd = () => {
-                        slider.classList.remove(direction);
-                    };
-
-                    slider.addEventListener('animationend', handleAnimationEnd, { once: true });
-                }
-            };
-
-            if (nextBtn && prevBtn) {
-                nextBtn.addEventListener('click', () => moveSlider('next'));
-                prevBtn.addEventListener('click', () => moveSlider('prev'));
-            }
-
-            return () => {
-                if (nextBtn && prevBtn) {
-                    nextBtn.removeEventListener('click', () => moveSlider('next'));
-                    prevBtn.removeEventListener('click', () => moveSlider('prev'));
-                }
-            };
-        }
-    }, [isLoading, selectedOrg]);
-
-    if (!selectedOrg || Object.keys(selectedOrg).length === 0) {
-        return <div>No organization data found.</div>;
-    }
+    }, []);
 
     return (
-        <div>
-            <div className="slider">
-                <div className="list">
-                    <div key={selectedOrg._id} className="item">
-                        <p>{selectedOrg.address}</p>
-                        <p>{selectedOrg.phone}</p>
-                    </div>
-                    <div className="item">
-                        <img src={ImgDefault} alt="Slider" />
-                        <div className="content">
-                            <div className="title">{selectedOrg.name}</div>
-                            <div className="description">
-                            <p>{selectedOrg.description}</p>
-                            </div>
-                            
-                            <div className="button">
-                                <button>UNIRME</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <body className='body-infoxd'>  
+        <header className='headers'>
+            <img className='bg' src={ImgDefault} alt="" />
+            <img className='esp'  alt="" />
+            <div className='volu containe'>
+                <a href="#" className='logoWaos'>HeartReach</a>   
+            </div>
 
-                <div className="thumbnail">
-                    {[1, 2, 3, 4].map((index) => (
-                        <div className="item" key={index}>
-                            <img src={ImgDefault} alt={`Thumbnail ${index}`} />
-                        </div>
-                    ))}
-                </div>
+            <div className='header-content containe'>
 
-                <div className="nextPrevArrows">
-                    <button className="prev"> &lt; </button>
-                    <button className="next"> &gt; </button>
+                <div className='header-infoxd'>
+
+                    <div className='header-txt'>
+                        <h1>{selectedOrg.name}</h1>
+                        <p>
+                            {selectedOrg.description}
+                        </p>
+                        <a href="#" className='btn-1'>Unirme</a>
+                    </div>
+                    <div className='header-img'>
+                        <img src={ImgWaos} alt="" />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        </header>
+        <section className='info containe'>
+            <div className='info-1'>
+                <img src={timexD} alt="" />
+                <h3>Role</h3>
+                <p>{selectedOrg.role}</p>
+            </div>
+
+            <div className='info-1'>
+                <img src={UbixD} alt="" />
+                <h3>Ubicacion</h3>
+                <p>{selectedOrg.address}</p>
+            </div>
+
+            <div className='info-1'>
+                <img src={PhonexD} alt="" />
+                <h3>Telefono</h3>
+                <p>{selectedOrg.phone}</p>
+            </div>
+        </section>
+        </body>
+        
+    )
 };
